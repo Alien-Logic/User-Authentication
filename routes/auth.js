f4eedb3c11af68aa85494f940bb1c3675ad1f0fe
123Aliean <thejaswinice21@gmail.com>
2024-05-10 23:59:26 +0530

    added view profile api

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,25 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require("../models/user");
 
+const authenticateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  const tokenParts = token.split(' ');
+  if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+    return res.status(401).json({ message: 'Invalid token format' });
+  }
+  const accessToken = tokenParts[1];
+  jwt.verify(accessToken, 'secret', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 router.get("/hello", (req, res) => {
     res.status(200).json("Hello World");
 })
@@ -51,7 +70,7 @@ router.post('/login', async (req, res) => {
       }
   
       const token = jwt.sign({ userId: user._id }, 'secret', { expiresIn: '1h' });
-      res.status(200).json({ message: "Logged in successfully" });
+      res.status(200).json({ message: "Logged in successfully", token: token });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Internal server error' });
@@ -60,6 +79,24 @@ router.post('/login', async (req, res) => {
 
 router.post('/signout', (req, res) => {
     res.clearCookie('token').json({ message: 'Signed out successfully' });
+});
+
+router.get('/profile', authenticateToken, async (req, res) => {
+  try {
+    console.log(req)
+    const email = req.user.email;
+
+    const user = await User.findOne(email);
+    console.log(user);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 module.exports = router;
