03f5e31909bc9b39c6e59c617a5e93c1a05a7003
123Aliean <thejaswinice21@gmail.com>
2024-05-10 23:29:16 +0530

    login api added

--- routes/auth.js
+++ routes/auth.js
@@ -34,6 +34,28 @@ router.post("/register", async (req, res) => {
         console.error(error);
         res.status(500).json({message: 'Inputs invalid', err: error});
     }
-})
+});
+
+router.post('/login', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+      }
+  
+      const token = jwt.sign({ userId: user._id }, 'secret', { expiresIn: '1h' });
+      res.status(200).json({ message: "Logged in successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
 
 module.exports = router;
